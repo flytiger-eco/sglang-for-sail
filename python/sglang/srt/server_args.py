e3a9fabe1aba640e44743999cd07e5ab5ed7755f
Vinsmoke86 <747930242@qq.com>
2026-06-22 12:52:08 +0800

    fix(dpsk v4): disable topk_v2 on ppu by default

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -1366,6 +1366,9 @@ class ServerArgs:
             # use cuda fla by default on ppu
             if not envs.SGLANG_SAIL_FLA_CUDA.is_set():
                 envs.SGLANG_SAIL_FLA_CUDA.set(True)
+            # disable deepseek v4 topk_v2 on PPU
+            if not envs.SGLANG_OPT_USE_TOPK_V2.is_set():
+                envs.SGLANG_OPT_USE_TOPK_V2.set(False)
 
     def _handle_piecewise_cuda_graph(self):
         # Skip auto-disable when enforce flag is set (for testing)
