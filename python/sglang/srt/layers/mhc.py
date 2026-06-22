fix(dpskv4/pro/mhc): disable deep_gemm.tf32_hc_prenorm_gemm split-k and fix DSV4_Pro W8A8 precision.

python/sglang/srt/layers/mhc.py
@@ -10,6 +10,7 @@ from sglang.jit_kernel.utils import is_arch_support_pdl
 from sglang.srt.environ import envs
 from sglang.srt.layers.attention.dsa.utils import is_dsa_prefill_cp_round_robin_split
 from sglang.srt.layers.utils.common import strict_contiguous
+from sglang.srt.utils import is_ppu
 
 logger = logging.getLogger(__name__)
 
@@ -884,12 +885,19 @@ def _mhc_pre_impl(
     )
 
     if envs.SGLANG_OPT_DEEPGEMM_HC_PRENORM.get():
-        n_splits = _compute_num_split_for_mhc_pre(num_tokens, hc_hidden_size)
+        if is_ppu():
+            assert (
+                n_splits == 1
+            ), "PPU version deep_gemm.tf32_hc_prenorm_gemm doesn't support split-k"
+        else:
+            n_splits = _compute_num_split_for_mhc_pre(num_tokens, hc_hidden_size)
 
-        gemm_out_mul = torch.empty(
+        # NOTE(PPU): Need to use torch.zeros instead of torch.empty here
+        # to fix precision problem on PPU SM80 with DeepSeek V4 Pro W8A8
+        gemm_out_mul = torch.zeros(
             n_splits, num_tokens, hc_mult3, dtype=torch.float32, device=residual.device
         )
-        gemm_out_sqrsum = torch.empty(
+        gemm_out_sqrsum = torch.zeros(
             n_splits, num_tokens, dtype=torch.float32, device=residual.device
         )
 