3d058aa7f6b3dd352bd5b901a02cf69972a27779
Vinsmoke <747930242@qq.com>
2026-07-13 17:40:07 +0800

    fix(quant fp8): clamp Nan scale_inv to 0 to avoid topk core dump

--- sgl-kernel/csrc/gemm/per_token_quant_fp8.cu
+++ sgl-kernel/csrc/gemm/per_token_quant_fp8.cu
@@ -166,7 +166,7 @@ __global__ void per_token_quant_fp8_small_batch_kernel(
   }
   __syncthreads();
 
-  const float scale_inv = 1.0f / scale;
+  const float scale_inv = (scale == 0.f) ? 0.f : 1.0f / scale;
 
   // Quantize using vectorized loads
   for (int32_t i = tid; i < num_vec_elems; i += block_dim) {
