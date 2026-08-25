bf9c46342a42d3720d1b3cc48cfa4654d4804500
shiyunzhi <shiyunzhi.syz@alibaba-inc.com>
2026-08-25 09:56:12 +0800

    fix(ppu-ci): drop unused register_ppu_ci import after registration removal

--- test/registered/breakable_cuda_graph/test_breakable_cuda_graph.py
+++ test/registered/breakable_cuda_graph/test_breakable_cuda_graph.py
@@ -12,7 +12,7 @@ import unittest
 import torch
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ci.ci_register import register_cuda_ci, register_ppu_ci
+from sglang.test.ci.ci_register import register_cuda_ci
 from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
